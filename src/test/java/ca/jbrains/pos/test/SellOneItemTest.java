e07d7e5e2dc412e9eae078caa1e233c33518ad11
J. B. Rainsberger <me@jbrains.ca>
2016-11-23 15:43:46 +0100

    We can sell two items, as long as we know exactly which items they are.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -7,22 +7,47 @@ public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("EUR 7.95", display.getText());
     }
 
+    @Test
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("EUR 12.50", display.getText());
+    }
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "EUR 7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
+
     public static class Sale {
-        public void onBarcode(String barcode) {
+        private Display display;
 
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) {
+            if ("12345".equals(barcode))
+                display.setText("EUR 7.95");
+            else
+                display.setText("EUR 12.50");
         }
     }
-
 }
