525ae9fe8e25141c4d70de7411eacb56d5b8b751
J. B. Rainsberger <me@jbrains.ca>
2016-11-24 14:09:35 +0100

    Our Controller can sell one item, as long as the Catalog can find its barcode.

---
+++ src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -0,0 +1,62 @@
+package ca.jbrains.pos.test;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class SellOneItemControllerTest {
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+
+    @Test
+    public void productFound() throws Exception {
+        final Catalog catalog = context.mock(Catalog.class);
+        final Display display = context.mock(Display.class);
+        final Price foundPrice = Price.cents(795);
+
+        context.checking(new Expectations() {{
+            allowing(catalog).findPrice(with("12345"));
+            will(returnValue(foundPrice));
+
+            oneOf(display).displayPrice(with(foundPrice));
+        }});
+
+        final SellOneItemController controller = new SellOneItemController(catalog, display);
+        controller.onBarcode("12345");
+    }
+
+    public interface Catalog {
+        Price findPrice(String barcode);
+    }
+
+    public interface Display {
+        void displayPrice(Price price);
+    }
+
+    public static class SellOneItemController {
+        private final Catalog catalog;
+        private final Display display;
+
+        public SellOneItemController(Catalog catalog, Display display) {
+            this.catalog = catalog;
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) {
+            display.displayPrice(catalog.findPrice(barcode));
+        }
+    }
+
+    public static class Price {
+        public static Price cents(int centsValue) {
+            return new Price();
+        }
+
+        @Override
+        public String toString() {
+            return "a Price";
+        }
+    }
+
+}
