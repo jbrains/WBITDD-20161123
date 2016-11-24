f87c41721bb135a76bc0e5979037fa30f875092b
J. B. Rainsberger <me@jbrains.ca>
2016-11-24 16:20:17 +0100

    The in-memory Catalog now gracefully handles not finding a barcode.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -15,6 +15,13 @@ public class FindPriceInMemoryCatalogTest {
         Assert.assertEquals(foundPrice, catalog.findPrice("12345"));
     }
 
+    @Test
+    public void productNotFound() throws Exception {
+        final InMemoryCatalog catalog = new InMemoryCatalog(
+                Collections.emptyMap());
+        Assert.assertEquals(null, catalog.findPrice("12345"));
+    }
+
     public static class InMemoryCatalog {
         private final Map<String, Price> pricesByBarcode;
 
