205848b27785755427c835e784c7d9a8218bed18
J. B. Rainsberger <me@jbrains.ca>
2016-11-25 09:57:22 +0100

    We now confirm that we format any Price the way we want it to look for English.

--- src/test/groovy/ca/jbrains/pos/test/FormatPriceTest.groovy
+++ src/test/groovy/ca/jbrains/pos/test/FormatPriceTest.groovy
@@ -9,9 +9,14 @@ class FormatPriceTest extends Specification {
         new EnglishLanguageMessageFormat().format(price) == text
 
         where:
-        price             || text
-        Price.cents(795)  || "EUR 7.95"
-        Price.cents(1265) || "EUR 12.65"
-        Price.cents(850)  || "EUR 8.50"
+        price                || text
+        Price.cents(795)     || "EUR 7.95"
+        Price.cents(1265)    || "EUR 12.65"
+        Price.cents(850)     || "EUR 8.50"
+        Price.cents(1700)    || "EUR 17.00"
+        Price.cents(0)       || "EUR 0.00"
+        Price.cents(1)       || "EUR 0.01"
+        Price.cents(70)      || "EUR 0.70"
+        Price.cents(2837124) || "EUR 28371.24"
     }
 }
